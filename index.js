2f62be3e6f321eb6321e4aa6502b2ce2ff6c4448
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-05-21 15:52:43 -0600

    created classes and test files

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const inquirer = require('inquirer'); // Package for prompting user input
 const fs = require('fs'); // File system module
 const { Console } = require('console');
+const circle = require('./lib/circle');
+const square = require('./lib/square');
+const triangle = require('./lib/triangle');
 const colors = [
     { name: "red",  value: "red"},
     { name: "blue", value: "blue"},
@@ -19,15 +22,19 @@ const questions = () => {
         { type: "list",  name: "shape",     message: "Select the shape for your logo",        choices: shapes,},
         { type: "list",  name: "shapeColor",message: "Select the shape's color for your logo",choices: colors,},
 ])}
-const svgContent = (answers) =>{
-    const shapes = require("./lib/shapes");
-    let svgContent;
-    switch(answers.shape){
-        case "circle":  svgContent = shapes.renderCircle(answers);   break;
-        case "square":  svgContent = shapes.renderSquare(answers);   break;
-        case "triangle":svgContent = shapes.renderTriangle(answers); break;
+const svgContent = ({text, textColor, shape, shapeColor}) =>{
+    const userShape = require("./lib/"+shape);
+    let logoElements;
+    switch(shape){
+        case "circle":  logoElements = new userShape(text, textColor, shapeColor);   break;
+        case "square":  logoElements = new userShape(text, textColor, shapeColor);   break;
+        case "triangle":logoElements = new userShape(text, textColor, shapeColor); break;
         default:console.log("Something went wrong! Please try again.");
     }
+    console.log(logoElements);
+    const svgContent = logoElements.render();
+    console.log(svgContent);
+    
     writeToFile(svgContent);
 }
 function writeToFile(svgContent) {
