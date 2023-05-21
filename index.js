729e506ce2b0f25635422f33eb9a7e72f9e10a77
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-05-20 21:18:53 -0600

    index code cleaned

--- index.js
+++ index.js
@@ -2,19 +2,16 @@
 const inquirer = require('inquirer'); // Package for prompting user input
 const fs = require('fs'); // File system module
 const { Console } = require('console');
-
 const colors = [
     { name: "red",  value: "red"},
     { name: "blue", value: "blue"},
     { name: "green",value: "green"},
 ];
-
 const shapes = [
     {name:"circle",  value:"circle"},
     {name:"square",  value:"square"},
     {name:"triangle",value:"triangle"},
 ]
-
 const questions = () => {
     return inquirer.prompt([
         { type: "input", name: "text",      message: "Enter text for the logo (must not be more than 3 characters)."},
@@ -22,36 +19,25 @@ const questions = () => {
         { type: "list",  name: "shape",     message: "Select the shape for your logo",        choices: shapes,},
         { type: "list",  name: "shapeColor",message: "Select the shape's color for your logo",choices: colors,},
 ])}
-
 const svgContent = (answers) =>{
     const shapes = require("./lib/shapes");
     let svgContent;
-
     switch(answers.shape){
-        case "circle":  svgContent = shapes.renderCircle(answers);
-        break;
-        case "square":  svgContent = shapes.renderSquare(answers);
-        break;
-        case "triangle":svgContent = shapes.renderTriangle(answers);
-        break;
-        default:  console.log("Something went wrong! Please try again.");
+        case "circle":  svgContent = shapes.renderCircle(answers);   break;
+        case "square":  svgContent = shapes.renderSquare(answers);   break;
+        case "triangle":svgContent = shapes.renderTriangle(answers); break;
+        default:console.log("Something went wrong! Please try again.");
     }
-
     writeToFile(svgContent);
 }
-
 function writeToFile(svgContent) {
     fs.writeFile("userLogo.svg",svgContent,(err) =>
     err ? console.log(err): console.log("Successfully created userLogo.svg")  
 )}
-
-
 function init(){
     questions()
     .then((answers) => {
         svgContent(answers);
     })
 }
-
-
 init(); // Function call to initialize app
